6aeb9162cc00d711b79fb6144b95ea07c1729b1c
KauaLibrelato <kaualibrelatodacosta@gmail.com>
2024-11-27 20:10:48 -0300

    feat: add new code

--- src/tests/Tests.spec.js
+++ src/tests/Tests.spec.js
@@ -1,20 +1,20 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { Rate } from 'k6/metrics';
+import { Rate, Trend } from 'k6/metrics';
 
-// Métrica personalizada para rastrear taxa de sucesso (status 200)
-export let successRate = new Rate('success_rate');
+export const getPersonsDuration = new Trend('get_persons_duration', true);
+export const rateStatusCodeOk = new Rate('rate_status_code_ok');
 
 export let options = {
   stages: [
     { duration: '1m', target: 10 },
-    { duration: '2m', target: 50 },
-    { duration: '1m', target: 150 },
+    { duration: '2m', target: 100 },
+    { duration: '1m', target: 200 },
     { duration: '1m', target: 300 }
   ],
   thresholds: {
-    success_rate: ['rate>=0.95'],
-    'http_req_duration{status:200}': ['p(95)<5700'],
+    http_req_duration: ['p(95)<5700'],
+    rate_status_code_ok: ['rate>0.95'],
     http_req_failed: ['rate<0.12']
   }
 };
@@ -22,7 +22,7 @@ export let options = {
 export default function () {
   const res = http.get('https://reqres.in/api/users?page=2');
 
-  successRate.add(res.status === 200);
+  rateStatusCodeOk.add(res.status === 200);
 
   check(res, {
     'status é 200': r => r.status === 200,
